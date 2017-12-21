cd076d1796adc03c00820117aced09e0b6d5865d
deced <shadow724156286@hotmail.com>
2017-12-21 19:54:12 +0800

    页脚

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,25 +8,27 @@
     <link rel="stylesheet" href="css/app.css">
     <title>@yield("title")</title>
     <style>
-        .navbar-header{
+        .navbar-header {
             margin-left: 15%;
             margin-right: 10%;
         }
 
-        .ejdh{
+        .ejdh {
             margin-left: 33%;
         }
-        .ejdh ul{
+
+        .ejdh ul {
             height: 60px;
         }
-        .ejdh ul a{
+
+        .ejdh ul a {
             padding-top: 15px;
             padding-bottom: 0;
             height: 100%;
         }
 
         .ejdh ul .active {
-            color:#e9f4f4;
+            color: #e9f4f4;
             border-bottom: #e9f4f4 solid 1px;
         }
 
@@ -34,40 +36,63 @@
             background-color: transparent !important;
         }
 
+        .footer{
+            background-color: #eee;
+        }
+
     </style>
 </head>
 <body>
-    <div class="container-fluid">
-       <nav class="navbar navbar-default" role="navigation">
-           <div class="navbar-header">
-               <img src="images/logo.png" alt="泾河城服">
-           </div>
-           <div>
-               <ul class="nav navbar-nav">
-                   <li>
-                       <a href="#" class="active">走进城服</a>
-                   </li>
-                   <li><a href="#">物业管理</a></li>
-                   <li><a href="#">招商运营</a></li>
-                   <li><a href="#">研学活动</a></li>
-                   <li><a href="#">文旅交流</a></li>
-                   <li><a href="#">定制服务</a></li>
-                   <li><a href="#">综合服务</a></li>
-               </ul>
-           </div>
-       </nav>
-        <div class="col-md-12 navbar-bg">
-            <div class="row">
-                <div class="col-md-6 ejdh">
-                    <ul class="nav navbar-nav" style="height: 50px;">
+<div class="container-fluid">
+    {{--导航栏--}}
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="navbar-header">
+            <img src="images/logo.png" alt="泾河城服">
+        </div>
+        <div>
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="#" class="active">走进城服</a>
+                </li>
+                <li><a href="#">物业管理</a></li>
+                <li><a href="#">招商运营</a></li>
+                <li><a href="#">研学活动</a></li>
+                <li><a href="#">文旅交流</a></li>
+                <li><a href="#">定制服务</a></li>
+                <li><a href="#">综合服务</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="col-md-12 navbar-bg">
+        <div class="row">
+            <div class="col-md-6 ejdh">
+                <ul class="nav navbar-nav" style="height: 50px;">
+                    @section('ejdh')
                         <li><a href="#" class="active">企业简介</a></li>
                         <li><a href="#">发展愿景</a></li>
                         <li><a href="#">组织架构</a></li>
-                    </ul>
-                </div>
+                    @show
+                </ul>
             </div>
         </div>
     </div>
+
+    {{--正文内容--}}
+    {{--@yield('contents)--}}
+
+    {{--页面底部--}}
+    <div class="footer">
+        
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <img src="images/ewm.jgp" alt="">
+            </div>
+        </div>
+        
+    </div>
+    
+</div>
+
 
 </body>
 </html>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.header')
+
+{{--@section('contents')--}}
+    {{--<h1>测试内容</h1>--}}
+{{--@endsection--}}
