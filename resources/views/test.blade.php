31a4ce9f334dc4208103f2af349d391d24899979
deced <shadow724156286@hotmail.com>
2018-01-24 23:00:47 +0800

    首页改版基本完成（不包含后台部分）

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -83,111 +83,121 @@
         </div>
     </div>
 
+    {{-- 特色党建、企业扶贫、团队建设 --}}
     <div class="row" style="margin-top: 50px; margin-bottom: 50px;">
-        <div class="col-md-2 col-md-offset-3">
-            <div class="thumbnail center-block djtp">
-                <img src="images/tp1.jpg" alt="..." style="width: 100%;">
-                <div class="caption text-center">
-                    <h3><a href="{{ url('/tsdj') }}">特色党建</a></h3>
-                    <p>做新时代有理想的共产党员</p>
-                </div>
-            </div>
-        </div>
+        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-md-offset-2">
+            <div class="row">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="thumbnail center-block djtp">
+                        <img src="images/tp1.jpg" alt="..." style="width: 100%;">
+                        <div class="caption text-center">
+                            <h3><a href="{{ url('/tsdj') }}">特色党建</a></h3>
+                            <p>围绕业务抓党建 抓好党建促发展</p>
+                        </div>
+                    </div>
 
-        <div class="col-md-2">
-            <div class="thumbnail center-block djtp">
-                <img src="images/tp2.jpg" alt="..." style="width: 100%;">
-                <div class="caption text-center">
-                    <h3><a href="{{ url('/qyfp') }}">企业扶贫</a></h3>
-                    <p>践行精准扶贫政策 加快城乡统筹发展</p>
                 </div>
-            </div>
-        </div>
 
-        <div class="col-md-2">
-            <div class="thumbnail center-block djtp">
-                <img src="images/tp3.jpg" alt="..." style="width: 100%;">
-                <div class="caption text-center">
-                    <h3><a href="{{ url('/tdjs') }}">团队建设</a></h3>
-                    <p>用激情和梦想去塑造城服人</p>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="thumbnail center-block djtp">
+                        <img src="images/tp2.jpg" alt="..." style="width: 100%;">
+                        <div class="caption text-center">
+                            <h3><a href="{{ url('/qyfp') }}">企业扶贫</a></h3>
+                            <p>践行精准扶贫政策 加快城乡统筹发展</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="thumbnail center-block djtp">
+                        <img src="images/tp3.jpg" alt="..." style="width: 100%;">
+                        <div class="caption text-center">
+                            <h3><a href="{{ url('/tdjs') }}">团队建设</a></h3>
+                            <p>科技创新服务 诚信实现双赢 专业成就精品</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
+    <div class="row" style="padding-top: 50px; padding-bottom: 50px;background-color: #eeeeee">
+        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-md-offset-2">
+            <div class="row">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                </div>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <img src="images/xwzx.png" alt="">
+                </div>
+            </div>
+
+            <div class="row" style="margin-top: 50px;margin-bottom: 50px;">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="swiper-container">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <a href="#"><img src="images/xw1.jpg" alt=""></a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="#"><img src="images/xw1.jpg" alt=""></a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="#"><img src="images/xw1.jpg" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 如果需要分页器 -->
+                    <div class="swiper-pagination fyq2"></div>
+                </div>
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+
+                    <ul class="nav nav-tabs nav-justified" role="tablist">
+                        <li role="presentation" class="active"><a href="#xxxq" aria-controls="#xxxq" role="tab"
+                                                                  data-toggle="tab">西咸新区新闻</a></li>
+                        <li role="presentation"><a href="#jhxc" aria-controls="jhxc" role="tab"
+                                                   data-toggle="tab">泾河新城新闻</a>
+                        </li>
+                        <li role="presentation"><a href="#jhcf" aria-controls="jhcf" role="tab"
+                                                   data-toggle="tab">泾河城服新闻</a>
+                        </li>
+                    </ul>
+
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="xxxq">
+                            <ul>
+                                <li><a href="">泾河新城2017年第45周空气质量</a></li>
+                                <li><a href="">关于开展泾河新城砂石场、白灰场</a></li>
+                                <li><a href="">泾河新城权责清单目录</a></li>
+                                <li><a href="">陕西省西咸新区泾河新城政务服务中心</a></li>
+                                <li><a href="">产为孵化基地规划公示牌</a></li>
+                            </ul>
+                        </div>
+
+                        <div role="tabpanel" class="tab-pane" id="jhxc">
+                            <ul>
+                                <li><a href="">泾河新城2017年第45周空气质量</a></li>
+                                <li><a href="">关于开展泾河新城砂石场、白灰场</a></li>
+                                <li><a href="">泾河新城权责清单目录</a></li>
+                                <li><a href="">产为孵化基地规划公示牌</a></li>
+                            </ul>
+                        </div>
+
+                        <div role="tabpanel" class="tab-pane" id="jhcf">
+                            <ul>
+                                <li><a href="">关于开展泾河新城砂石场、白灰场</a></li>
+                                <li><a href="">泾河新城权责清单目录</a></li>
+                                <li><a href="">陕西省西咸新区泾河新城政务服务中心</a></li>
+                                <li><a href="">产为孵化基地规划公示牌</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="row">
-        <div class="col-md-2 col-md-offset-5">
-            <img src="images/xwzx.png" alt="">
-        </div>
-    </div>
-
-    <div class="row" style="margin-top: 50px;margin-bottom: 50px;">
-        <div class="col-md-3 col-md-offset-3">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <a href="#"><img src="images/xw1.jpg" alt=""></a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="#"><img src="images/xw1.jpg" alt=""></a>
-                    </div>
-                    <div class="swiper-slide">
-                        <a href="#"><img src="images/xw1.jpg" alt=""></a>
-                    </div>
-                </div>
-            </div>
-            <!-- 如果需要分页器 -->
-            <div class="swiper-pagination fyq2"></div>
-        </div>
-
-        <div class="col-md-3 col-mg-offset-3">
-
-            <ul class="nav nav-tabs nav-justified" role="tablist">
-                <li role="presentation" class="active"><a href="#xxxq" aria-controls="#xxxq" role="tab"
-                                                          data-toggle="tab">西咸新区新闻</a></li>
-                <li role="presentation"><a href="#jhxc" aria-controls="jhxc" role="tab" data-toggle="tab">泾河新城新闻</a>
-                </li>
-                <li role="presentation"><a href="#jhcf" aria-controls="jhcf" role="tab" data-toggle="tab">泾河城服新闻</a>
-                </li>
-            </ul>
-
-            <div class="tab-content">
-                <div role="tabpanel" class="tab-pane active" id="xxxq">
-                    <ul>
-                        <li><a href="">泾河新城2017年第45周空气质量</a></li>
-                        <li><a href="">关于开展泾河新城砂石场、白灰场</a></li>
-                        <li><a href="">泾河新城权责清单目录</a></li>
-                        <li><a href="">陕西省西咸新区泾河新城政务服务中心</a></li>
-                        <li><a href="">产为孵化基地规划公示牌</a></li>
-                    </ul>
-                </div>
-
-                <div role="tabpanel" class="tab-pane" id="jhxc">
-                    <ul>
-                        <li><a href="">泾河新城2017年第45周空气质量</a></li>
-                        <li><a href="">关于开展泾河新城砂石场、白灰场</a></li>
-                        <li><a href="">泾河新城权责清单目录</a></li>
-                        <li><a href="">产为孵化基地规划公示牌</a></li>
-                    </ul>
-                </div>
-
-                <div role="tabpanel" class="tab-pane" id="jhcf">
-                    <ul>
-                        <li><a href="">关于开展泾河新城砂石场、白灰场</a></li>
-                        <li><a href="">泾河新城权责清单目录</a></li>
-                        <li><a href="">陕西省西咸新区泾河新城政务服务中心</a></li>
-                        <li><a href="">产为孵化基地规划公示牌</a></li>
-                    </ul>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-
-    <div class="row">
-        <div class="col-md-12" style="margin-top: 20px;">
+        <div class="col-md-12">
             <div class="swiper-container">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
@@ -212,59 +222,60 @@
     </div>
 
     <div class="row">
-        <div class="col-md-2 col-md-offset-5" style="margin-top: 50px;margin-bottom: 50px;">
-            <img src="images/fwdt.png" alt="">
-        </div>
-    </div>
-
-    <div class="row fwdt" style="margin-top: 50px; margin-bottom: 50px;">
-        <div class="col-md-2 col-md-offset-3">
-            <div class="thumbnail center-block djtp">
-                <img src="images/fw1.jpg" alt="..." style="width: 100%;">
-                <hr>
-                <div class="caption">
-                    <h3><a href="#">定制服务动态</a></h3>
-                    <ul>
-                        <li>泾河新城2017年第45周空气质量</li>
-                        <li>关于开展泾河新城砂石场、白灰场</li>
-                        <li>泾河新城权责清单目录</li>
-                        <li>陕西省西咸新区泾河新城政务服务中心</li>
-                        <li>产为孵化基地规划公示牌</li>
-                    </ul>
+        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 col-md-offset-2">
+            <div class="row">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-md-offset-4" style="margin-top: 100px;margin-bottom: 50px;">
+                    <img src="images/fwdt.png" alt="">
                 </div>
             </div>
-        </div>
-
-        <div class="col-md-2">
-            <div class="thumbnail center-block djtp">
-                <img src="images/fw2.jpg" alt="..." style="width: 100%;">
-                <hr>
-                <div class="caption">
-                    <h3><a href="#">研学活动动态</a></h3>
-                    <ul>
-                        <li>泾河新城2017年第45周空气质量</li>
-                        <li>关于开展泾河新城砂石场、白灰场</li>
-                        <li>泾河新城权责清单目录</li>
-                        <li>陕西省西咸新区泾河新城政务服务中心</li>
-                        <li>产为孵化基地规划公示牌</li>
-                    </ul>
+            <div class="row fwdt" style="margin-top: 50px; margin-bottom: 50px;">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="thumbnail center-block djtp">
+                        <img src="images/fw2.jpg" alt="..." style="width: 100%;">
+                        <hr>
+                        <div class="caption">
+                            <h3><a href="#">研学活动动态</a></h3>
+                            <ul>
+                                <li>泾河新城2017年第45周空气质量</li>
+                                <li>关于开展泾河新城砂石场、白灰场</li>
+                                <li>泾河新城权责清单目录</li>
+                                <li>陕西省西咸新区泾河新城政务服务中心</li>
+                                <li>产为孵化基地规划公示牌</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-
-        <div class="col-md-2">
-            <div class="thumbnail center-block djtp">
-                <img src="images/fw3.jpg" alt="..." style="width: 100%;">
-                <hr>
-                <div class="caption">
-                    <h3><a href="#">综合服务动态</a></h3>
-                    <ul>
-                        <li>泾河新城2017年第45周空气质量</li>
-                        <li>关于开展泾河新城砂石场、白灰场</li>
-                        <li>泾河新城权责清单目录</li>
-                        <li>陕西省西咸新区泾河新城政务服务中心</li>
-                        <li>产为孵化基地规划公示牌</li>
-                    </ul>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="thumbnail center-block djtp">
+                        <img src="images/fw1.jpg" alt="..." style="width: 100%;">
+                        <hr>
+                        <div class="caption">
+                            <h3><a href="#">定制服务动态</a></h3>
+                            <ul>
+                                <li>泾河新城2017年第45周空气质量</li>
+                                <li>关于开展泾河新城砂石场、白灰场</li>
+                                <li>泾河新城权责清单目录</li>
+                                <li>陕西省西咸新区泾河新城政务服务中心</li>
+                                <li>产为孵化基地规划公示牌</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <div class="thumbnail center-block djtp">
+                        <img src="images/fw3.jpg" alt="..." style="width: 100%;">
+                        <hr>
+                        <div class="caption">
+                            <h3><a href="#">综合服务动态</a></h3>
+                            <ul>
+                                <li>泾河新城2017年第45周空气质量</li>
+                                <li>关于开展泾河新城砂石场、白灰场</li>
+                                <li>泾河新城权责清单目录</li>
+                                <li>陕西省西咸新区泾河新城政务服务中心</li>
+                                <li>产为孵化基地规划公示牌</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
