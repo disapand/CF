64d04140c988574558762aed97c30a4da21f8405
deced <shadow724156286@hotmail.com>
2018-06-11 20:09:49 +0800

    调整间距

--- resources/views/zhfw/zhcs.blade.php
+++ resources/views/zhfw/zhcs.blade.php
@@ -44,7 +44,7 @@
         </div>
     </div>
 
-    <div class="row" style="margin-bottom: 3%;">
+    <div class="row" style="margin-bottom: 1%;">
         <div class="col-xs-10 col-sm-10 col-md-8 col-lg-8 col-md-offset-2 col-xs-offset-1 col-sm-offset-1 wzys">
             <p>智慧城市作为人类可持续发展的又一探索，在我国有着意义深远的影响。在西成新区建设过程中，运用信息和通信技术手段感测、分析、整合城市运行核心系统的各项关键信息，对包括民生、环保、公共安全、城市服务、工商业活动在内的各种需求做出智能响应。利用先进的信息技术，实现城市智慧式管理和运行，进而为城市中的人创造更美好的生活，促进城市的和谐、可持续成长是我们一直以来的愿望。</p>
         </div>
